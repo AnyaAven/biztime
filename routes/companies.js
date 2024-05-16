13e81581b34249c0ae2a1e40f6e843085ad83d38
Anya Aven <leafeon96@gmail.com>
2024-05-16 15:18:30 -0400

    Finished gathering all companies

--- routes/companies.js
+++ routes/companies.js
@@ -6,5 +6,19 @@ import { BadRequestError } from "../expressError.js";
 
 const router = express.Router();
 
+/** Get companies
+ *
+ * Returns {companies: [{code, name}, ...]}
+*/
+router.get("", async function(req, res){
+  const results = await db.query(
+    `SELECT code, name
+    FROM companies`);
+
+  const companies = results.rows;
+
+  return res.json({ companies });
+});
+
 
 export default router;
