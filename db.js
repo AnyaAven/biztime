febaa347ac24f0daccec97e9dfe24c5f915e3893
Anya Aven <leafeon96@gmail.com>
2024-05-16 15:05:26 -0400

    Finished DB connection

--- db.js
+++ db.js
@@ -1,2 +1,16 @@
 /** Database setup for BizTime. */
 
+import pg from "pg";
+const { Client } = pg;
+
+const DB_URI = process.env.NODE_ENV === "test"
+  ? "postgresql:///biztime"
+  : "postgresql:///biztime_test";
+
+const db = new Client({
+  connectionString: DB_URI,
+});
+
+await db.connect();
+
+export default db;
