2648d02f10a1f9ae169aba5181dcae625053d5cc
Anya Aven <leafeon96@gmail.com>
2024-05-16 20:42:47 -0400

    before and after set up with all companies test

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -1,1 +1,54 @@
-//TODO: Don't forget to db.end() after each test
+
+import { beforeEach, afterAll, describe, test, expect } from "vitest";
+import request from "supertest";
+import app from "../app.js";
+import db from "../db.js";
+
+// {id}, {id}, ...
+let testCompany1, testCompany2, testInvoice1, testInvoice2;
+
+beforeEach(async function (){
+  await db.query("DELETE FROM companies");
+  await db.query("DELETE FROM invoices");
+
+  const cResults = await db.query(`
+    INSERT INTO companies (code, name, description)
+    VALUES ('cCode1', 'cName1', 'cDescription1'),
+          ('cCode2', 'cName2', 'cDescription2')
+    RETURNING code`);
+
+  testCompany1 = cResults.rows[0];
+  testCompany2 = cResults.rows[1];
+
+  const iResults = await db.query(`
+    INSERT INTO invoices (comp_code, amt)
+    VALUES ('cCode1', 100),
+          ('cCode2', 200)
+    RETURNING id`);
+
+  testInvoice1 = iResults.rows[0];
+  testInvoice2 = iResults.rows[1];
+});
+
+//TODO: Can we use Date object for testing the current date.
+expect.any(String)
+
+describe("GET /companies", function (){
+
+  test("Get a list of companies", async function(){
+    const resp = await request(app).get("/companies");
+
+    expect(resp.body).toEqual(
+      {
+        companies: [
+          { code: 'cCode1', name: 'cName1' },
+          { code: 'cCode2', name: 'cName2' }
+        ]
+      }
+    )
+  })
+});
+
+afterAll(async function () {
+  await db.end();
+});
